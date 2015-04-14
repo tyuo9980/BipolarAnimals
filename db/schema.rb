45f95bcd1228a31630c69379722a3ac69442eab5
PlasticQA <testers@plasticmobile.com>
2015-04-14 18:15:45 -0400

    fixedddgit add -A

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150414215608) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string   "url",        null: false
     t.datetime "created_at", null: false
